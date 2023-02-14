c8a7d1d181c22d54bff5bf0a7b5ee9717d5fbe2c
RomanasV <romanasvenckus@gmail.com>
2023-02-14 10:26:35 +0200

    albums page functions created

--- albums.js
+++ albums.js
@@ -1,37 +1,44 @@
-fetch('https://jsonplaceholder.typicode.com/albums?_limit=30&_embed=photos&_expand=user')
-  .then(res => res.json())
-  .then(albums => {
-    const pageContent = document.querySelector('#page-content');
+async function init() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/albums?_limit=30&_embed=photos&_expand=user');
+  const albums = await res.json();
 
-    const albumsList = document.createElement('div');
-    albumsList.classList.add('albums-list');
+  const pageContent = document.querySelector('#page-content');
+  const albumsList = createAlbumsListElement(albums);
 
-    pageContent.append(albumsList);
+  pageContent.append(albumsList);
+}
 
-    albums.map(album => {
-      console.log(album)
-      const title = album.title;
-      const name = album.user.name;
-      const photosNumber = album.photos.length;
-      const randomIndex = Math.floor(Math.random() * album.photos.length);
-      const randomPhoto = album.photos[randomIndex];
+function createAlbumsListElement(albums) {
+  const albumsList = document.createElement('div');
+  albumsList.classList.add('albums-list');
 
-      const albumItem = document.createElement('div');
-      albumItem.classList.add('album-item');
+  albums.map(album => {
+    const title = album.title;
+    const name = album.user.name;
+    const photosNumber = album.photos.length;
+    const randomIndex = Math.floor(Math.random() * album.photos.length);
+    const randomPhoto = album.photos[randomIndex];
 
-      const albumItemLink = document.createElement('a');
-      albumItemLink.href = './album.html';
+    const albumItem = document.createElement('div');
+    albumItem.classList.add('album-item');
 
-      const photoElement = document.createElement('img');
-      photoElement.src = randomPhoto.thumbnailUrl;
-      photoElement.title = randomPhoto.title;
+    const albumItemLink = document.createElement('a');
+    albumItemLink.href = './album.html';
 
-      const albumTitle = document.createElement('h2');
-      albumTitle.textContent = `${title} (${photosNumber}), author: ${name}`;
-      
-      albumItemLink.append(photoElement, albumTitle);
-      albumItem.append(albumItemLink);
-      
-      albumsList.append(albumItem);
-    })
+    const photoElement = document.createElement('img');
+    photoElement.src = randomPhoto.thumbnailUrl;
+    photoElement.title = randomPhoto.title;
+
+    const albumTitle = document.createElement('h2');
+    albumTitle.textContent = `${title} (${photosNumber}), author: ${name}`;
+    
+    albumItemLink.append(photoElement, albumTitle);
+    albumItem.append(albumItemLink);
+    
+    albumsList.append(albumItem);
   })
+
+  return albumsList;
+}
+
+init();
