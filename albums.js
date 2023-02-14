4544c0516ed1e2c8a9ac772fed23d7875908cb8f
RomanasV <romanasvenckus@gmail.com>
2023-02-14 14:33:29 +0200

    header created

--- albums.js
+++ albums.js
@@ -7,9 +7,11 @@ async function init() {
   }
 
   const pageContent = document.querySelector('#page-content');
+  const header = createPageMainHeader();
   const albumsList = createAlbumsListElement(albums);
 
   pageContent.append(albumsList);
+  pageContent.before(header);
 }
 
 function createAlbumsListElement(albums) {
@@ -48,6 +50,57 @@ function createAlbumItemElement(album) {
   albumItem.append(albumItemLink);
   
   return albumItem;
+}
+
+function createPageMainHeader() {
+  const header = document.createElement('header');
+  const nav = document.createElement('nav');
+  nav.classList.add('main-navigation');
+  const menuList = document.createElement('ul');
+  menuList.classList.add('menu');
+
+  const menuItems = [
+    {
+      title: 'Home',
+      path: 'index.html',
+    },
+    {
+      title: 'Posts',
+      path: 'posts.html',
+    },
+    {
+      title: 'Users',
+      path: 'users.html',
+    },
+    {
+      title: 'Albums',
+      path: 'albums.html',
+    },
+  ];
+
+  menuItems.forEach(menuItem => {
+    // let title = menuItem.title;
+    // let path = menuItem.path;
+    let { title, path } = menuItem;
+
+    const menuItemElement = document.createElement('li');
+    menuItemElement.classList.add('menu-item');
+
+    if (location.pathname === '/' + path) {
+      menuItemElement.classList.add('active');
+    }
+
+    const menuLink = document.createElement('a');
+    menuLink.textContent = title;
+    menuLink.href = './' + path;
+
+    menuItemElement.append(menuLink);
+    menuList.append(menuItemElement);
+  })
+
+  nav.append(menuList);
+  header.append(nav);
+  return header;
 }
 
 init();
--- posts.js
+++ posts.js
@@ -6,6 +6,7 @@ async function init() {
   const header = createPageMainHeader();
   const postsList = createPostsListElement(posts);
   pageContent.append(postsList);
+  pageContent.before(header);
 }
 
 function createPostsListElement(posts) {
@@ -35,7 +36,54 @@ function createPostsListElement(posts) {
 }
 
 function createPageMainHeader() {
-  return 'Header';
+  const header = document.createElement('header');
+  const nav = document.createElement('nav');
+  nav.classList.add('main-navigation');
+  const menuList = document.createElement('ul');
+  menuList.classList.add('menu');
+
+  const menuItems = [
+    {
+      title: 'Home',
+      path: 'index.html',
+    },
+    {
+      title: 'Posts',
+      path: 'posts.html',
+    },
+    {
+      title: 'Users',
+      path: 'users.html',
+    },
+    {
+      title: 'Albums',
+      path: 'albums.html',
+    },
+  ];
+
+  menuItems.forEach(menuItem => {
+    // let title = menuItem.title;
+    // let path = menuItem.path;
+    let { title, path } = menuItem;
+
+    const menuItemElement = document.createElement('li');
+    menuItemElement.classList.add('menu-item');
+
+    if (location.pathname === '/' + path) {
+      menuItemElement.classList.add('active');
+    }
+
+    const menuLink = document.createElement('a');
+    menuLink.textContent = title;
+    menuLink.href = './' + path;
+
+    menuItemElement.append(menuLink);
+    menuList.append(menuItemElement);
+  })
+
+  nav.append(menuList);
+  header.append(nav);
+  return header;
 }
 
 init();
